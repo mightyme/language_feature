b4359b0e04e8b9f3150b31ed0bc2a93fcc5c3de6
瞿稻 <qudao@meizu.com>
2015-01-23 11:08:31 +0800

    Pointer: sample of ** pointer assignment Signed-off-by: 瞿稻 <qudao@meizu.com>

--- pointer.c
+++ pointer.c
@@ -1,51 +1,19 @@
 #include <stdio.h>
-struct my {
-    int mem_num;
-    struct my *member[0];
-};
-struct you {
-    int mem_num;
-    struct you *member;
-};
+#include <stdlib.h>
 
-struct he{
-    int mem_num;
-    struct he *member[];
-};
-int main(int argc, char ** argv)
+int main(int argc, char **argv)
 {
-    char *a = malloc(2*sizeof(char));
-    a[0] = 'q';
-    a[1] = 'd';
-    printf("%c, %c\n", a[0], a[1]);
-    free(a);
-    printf("%c, %c\n", a[0], a[1]);
-//    free(a);
-        
-	printf("sizeof char, short, int, long and long long is: %d %d %d %d and %d\n",
-            sizeof(char), sizeof(short), sizeof(int), sizeof(long), sizeof(long long));
-    printf("sizeof(struct my) is %d, sizeof(struct you) is %d, sizeof(struct he): %d\n",
-            sizeof(struct my), sizeof(struct you), sizeof(struct he));
-    struct my *my = malloc(sizeof(struct my));
-    my->mem_num = 2;
-#if 1
-    //my->member = malloc(my->mem_num * sizeof(struct my));//error!
-    my->member[0] = malloc(my->mem_num * sizeof(struct my));//error!
-    //my->member[1] = malloc(my->mem_num * sizeof(struct my));//error!
-    my->member[0][0].mem_num = 4;
-    my->member[0][1].mem_num = 5;
-    printf("%d, %d, %d\n", my->mem_num, my->member[0][0].mem_num, my->member[0][1].mem_num);
-    free(my);
-//    free(my);
-    free(my->member[0]);
-    free(my);
-    my->mem_num = 6;
-    printf("%d, %d, %d\n", my->mem_num, my->member[0][0].mem_num, my->member[0][1].mem_num);
-#endif
-    struct he *he = malloc(sizeof(struct he) + 3*(sizeof(struct he *)));
-    struct he he_1;
-    he_1.mem_num = 99;
-    he->member[2] = &he_1;
-    printf("%d\n", he->member[2]->mem_num);
+	int *p2 = 0xff;
+	int **pp;
+	printf("p2:0x%p, &p2:0x%p\n", p2, &p2);
+	printf("pp:0x%p, &*pp:0x%p, *pp:0x%p, &pp:0x%p, sizeof(*pp):%d\n", pp, &*pp, *pp, &pp, sizeof(*pp));
+
+	pp = malloc(sizeof(*pp));
+	printf("pp:0x%p, *pp:0x%p, &pp:0x%p, sizeof(**pp):%d\n", pp, *pp, &pp, sizeof(**pp));
+	p2 = malloc(sizeof(int));
+	printf("p2:0x%p, &p2:0x%p, sizeof(int):%d\n", p2, &p2, sizeof(int));
+	
+	*pp = p2;
+	printf("pp:0x%p, &*pp:0x%p, *pp:0x%p, &pp:0x%p\n", pp, &*pp, *pp, &pp);
 	return 0;
 }
